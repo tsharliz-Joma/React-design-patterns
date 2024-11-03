d379aa6ce2504a6885ac3d59e7d9a3c6ee6d0983
charles joma <charles.joma@chepnetwork.com>
2024-11-03 19:48:37 +1100

    DATASOURCE:: Add the data source component that handles of the data fetching

---
+++ containercomponents/serverinstructions/src/DataSource.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+// This component will be used to fetch data from the server and pass it down to its children.
+// This componenent is highly reusable and can be used to fetch any type of data from the server.
+export const DataSource = ({ getDataFunc = () => {}, resourceName, children }) => {
+  const [state, setState] = useState(null);
+  // The getDataFunc is a function that will be passed as a prop to the DataSource component.
+
+  useEffect(() => {
+    (async () => {
+      const data = await getDataFunc();
+      setState(data);
+    })();
+  }, [getDataFunc]);
+  
+  return (
+    <> 
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child, { [resourceName]: state });
+        }
+        return child;
+      })}
+    </>
+  );
+};
