71da73c7037536fe170befef4bb2d2757161565d
charles joma <charles.joma@chepnetwork.com>
2024-11-04 20:03:09 +1100

    move to personal laptop

--- containercomponents/serverinstructions/src/App.js
+++ containercomponents/serverinstructions/src/App.js
@@ -6,12 +6,22 @@ const getServerData = (url) => async () => {
   const response = await axios.get(url);
   return response.data;
 };
+const getLocalStorageData = (key) => () => {
+  return localStorage.getItem(key);
+};
+
+const Text = ({ message }) => {
+  return <h2>{message}</h2>;
+};
 
 function App() {
   return (
     <>
       <DataSource getDataFunc={getServerData("/users/456")} resourceName="user">
         <UserInfo />
+      </DataSource>
+      <DataSource getDataFunc={getLocalStorageData("message")} resourceName={"message"}>
+        <Text />
       </DataSource>
     </>
   );
